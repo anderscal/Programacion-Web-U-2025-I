8e41f5bb0ad5f83e0693931312f77309571aa944
anderscal <calderonanderson78@gmail.com>
2025-04-03 09:37:37 -0500

    iniciando ejercicio api1

---
+++ API_REACTv1/src/Components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css'
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+const Header = () => {
+    return (
+        <>
+            <header>
+                <GitHubIcon id="icon-git"/>
+                <h1>User</h1>
+                <h2>GitHub</h2>
+            </header>
+        </>
+
+
+
+    )
+}
+
+export default Header
