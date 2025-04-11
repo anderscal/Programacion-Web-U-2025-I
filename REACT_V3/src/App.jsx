becdb7f3a8ec3ad7788c17342d314ab05035e1eb
anderscal <calderonanderson78@gmail.com>
2025-04-10 22:22:17 -0500

    paginando con browserrouter

--- REACT_V3/src/App.jsx
+++ REACT_V3/src/App.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom'
 import './App.css'
 import Footer from './Components/Footer/Footer'
 import Header from './Components/Header/Header'
 import NavBar from './Components/NavBar/NavBar'
 import HomePage from './Pages/HomePage/HomePage'
+import DetailsPage from './Pages/DetailsPage/DetailsPage'
 
 const App = () => {
   return (
-    <div>
+    <>
       <Header/>
       <NavBar/>
-      <HomePage/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/details/" element={<DetailsPage />} />
+        </Routes>
+      </BrowserRouter>
+  
       <Footer />
-    </div>
+    </>
   )
 }
 
---
+++ REACT_V3/src/Pages/DetailsPage/DetailsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DetailsPage = () => {
+    return (
+        <div>
+            <h1>Rick and Morty</h1>
+            <p>Welcome to the Rick and Morty API</p>
+        </div>
+    )
+}
+
+export default DetailsPage
